5c4f8e25d7a3a3e2103e2e8e3f47c919133f0c56
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-15 18:03:59 -0700

    Setup theme for external components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const darkTheme = createTheme({ palette: { mode: "dark" } });
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <ThemeProvider theme={darkTheme}>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
