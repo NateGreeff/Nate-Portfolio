4802f8e662b75c95ec2f66a65956c6ef90df09b5
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-16 16:47:21 -0700

    Cleaned up footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,29 @@
-import './Footer.css'
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import "./Footer.css";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function Footer() {
-    return (
-        <footer>
-            <div className='background'>
-                <LinkedInIcon className='icon' />
-                <GitHubIcon className='icon' />
-                <TwitterIcon className='icon' />
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="background">
+        <Tooltip title="Visit My LinkedIn" arrow>
+          <a href="https://www.linkedin.com/in/nathangreeff">
+            <LinkedInIcon className="icon" />
+          </a>
+        </Tooltip>
+        <Tooltip title="Visit My Github" arrow>
+          <a href="https://github.com/NateGreeff">
+            <GitHubIcon className="icon" />
+          </a>
+        </Tooltip>
+        <Tooltip title="Visit My YouTube Channel" arrow>
+          <a href="https://www.youtube.com/channel/UCIwCO5H0NVQnPApCukIpShQ">
+            <YouTubeIcon className="icon" />
+          </a>
+        </Tooltip>
+      </div>
+    </footer>
+  );
 }
