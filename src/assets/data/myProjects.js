021a61baaa3a262b4263a47e73775a5468f3f312
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-15 18:06:51 -0700

    Project components and data

---
+++ src/assets/data/myProjects.js
@@ -0,0 +1,22 @@
+const projectData = [
+    {
+        name: "Utah Real Estate Admin Rules",
+        live: "https://re-law-lookup-production-6926.up.railway.app/",
+        github: "https://github.com/Jelu113/RE-law-lookup",
+        image: "https://raw.githubusercontent.com/Jelu113/RE-law-lookup/main/public/img/HomePage.jpg"
+    },
+    {
+        name: "The Date Planner",
+        live: "https://greeny467.github.io/date-planning-project-website/",
+        github: "https://github.com/Greeny467/date-planning-project-website",
+        image: "https://user-images.githubusercontent.com/21099065/258675578-27000e63-5d47-4c2e-b32a-2a2afaa1b49b.png"
+    },
+    {
+        name: "The Coding Quiz",
+        live: "https://nategreeff.github.io/The-Coding-Quiz/",
+        github: "https://github.com/NateGreeff/The-Coding-Quiz",
+        image: "https://github.com/NateGreeff/The-Coding-Quiz/raw/main/image.png"
+    }
+]
+
+export default projectData;
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,23 @@
+import './Project.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Tooltip from '@mui/material/Tooltip';
+
+
+function Project(props) {
+  const { name, image, github, live } = props;
+  return (
+    <div className="project background">
+      <a href={live} target='_blank' rel='noreferrer'>
+        <img src={image} alt={name} />
+      </a>
+      <div className="projectInfo">
+        <h3>{name}</h3>
+        <Tooltip title="Visit Repo" arrow>
+          <a href={github} target='_blank' rel='noreferrer'><GitHubIcon className="githubIcon" fontSize='large'/></a>
+        </Tooltip>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
