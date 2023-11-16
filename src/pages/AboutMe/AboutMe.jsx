cbce5a733fc46b87d8de0e9e82347c03a582ee43
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-15 18:05:07 -0700

    Cleaned up about me section

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -4,26 +4,16 @@ export default function AboutMe() {
     return (
         <>
             <section className="aboutContainer">
-                <section className="textSection about">
-                    <p>
-                        Welcome to my portfolio!
-                        <br /><br />
-                        I'm Nathan Greeff, an aspiring Full Stack Developer currently deepening my knowledge and skills in the technology.
-                        <br /><br />
-                        As a student in this field, I'm gaining comprehensive insight into complex systems, enabling me to construct intricate software solutions, both on the front-end and back-end.
-                        <br /><br />
-                        In addition to my practical training, I am fervently dedicated to ongoing learning and personal development.
-                        Through selective coursework, I've been able to broaden my technical expertise, focusing on aspects like technology project management, process enhancement, and advanced coding techniques.
-                        This education is integral in forming my holistic viewpoint on web development.
-                        <br /><br />
-                        In my portfolio, you'll see a variety of projects that showcase my budding skills as a Full Stack Developer.
-                        These projects are a testament to my potential in crafting innovative and efficient solutions, ready to face the dynamic challenges of the tech industry head-on.
-                        Each one is a representation of my commitment to leveraging and honing my technical capabilities to foster innovation.
-                        <br /><br />
-                        I hope you enjoy exploring my work!
-                    </p>
+                <section className="textSection about background">
+                    <h3>Welcome to my portfolio!</h3>
+                        <p>I'm Nathan Greeff, a Full Stack Developer with a passion for both learning and applying cutting-edge web technologies.</p>
+                        <p>Having completed extensive training, I possess a robust skill set covering both front-end and back-end development. My journey from student to professional has equipped me with the tools to build complex, scalable, and responsive web applications.</p>
+                        <p>My commitment to continuous learning and professional growth remains steadfast. I actively engage in expanding my expertise through advanced courses and real-world projects, focusing on areas such as efficient code practices, latest industry trends, and innovative solution development.</p>
+                        <p>My portfolio showcases a diverse array of projects that reflect my journey and growth as a Full Stack Developer. These projects demonstrate my ability to innovate and adapt, creating impactful and efficient web solutions suited for the ever-evolving demands of the technology sector.</p>
+                        <p>Each project is a chapter in my ongoing story of development and growth, illustrating my dedication to mastering the art of web development and my readiness to contribute significantly to the field.</p>
+                        <p>Enjoy exploring my work, and witness the evolution of a dedicated Full Stack Developer!</p>
                 </section>
-                <section className="imgSection about">
+                <section className="imgSection about background">
                     <img src="src/assets/img/Photo of Nathan.png" alt="Picture of Nate" />
                 </section>
             </section>
