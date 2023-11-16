5bfd954054929c7f65072f17a2867d663c4205e6
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-16 16:59:28 -0700

    testing netlify forms

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,53 +1,51 @@
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
 import "./ContactForm.css";
 import { TextField, Button } from "@mui/material/";
 
 export default function ContactForm({ ...props }) {
 
-  const [formInput, setFormInput] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+  // const [formInput, setFormInput] = useState({
+  //   name: "",
+  //   email: "",
+  //   message: "",
+  // });
 
-  const [formData, setFormData] = useState({});
+  // const [formData, setFormData] = useState({});
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    setFormData(formInput);
-    setFormInput({
-      name: "",
-      email: "",
-      message: "",
-    });
-  }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   setFormData(formInput);
+  //   setFormInput({
+  //     name: "",
+  //     email: "",
+  //     message: "",
+  //   });
+  // }
 
-  function handleInputChange({target: {name, value}}) {
-    setFormInput({...formInput, [name]: value});
-  }
+  // function handleInputChange({target: {name, value}}) {
+  //   setFormInput({...formInput, [name]: value});
+  // }
 
-  useEffect(() => {
-    console.log(formData);
-  }, [formData]);
+  // useEffect(() => {
+  //   console.log(formData);
+  // }, [formData]);
 
   return (
-    <form netlify {...props} action="">
+    <form name="contact" data-netlify="true" {...props} action="">
       <span id="requiredText">* Required</span>
       <h1 className="contactTitle">Contact Me</h1>
-      <TextField id="nameInput" onChange={handleInputChange} label="Name" name="name" value={formInput.name} fullWidth required />
-      <TextField id="emailInput" onChange={handleInputChange} label="Email" name="email" value={formInput.email} fullWidth required />
+      <TextField id="nameInput" label="Name" name="name" fullWidth required />
+      <TextField id="emailInput" label="Email" name="email" fullWidth required />
       <TextField
         id="messageInput"
-        onChange={handleInputChange}
         name="message"
-        value={formInput.message}
         label="Message"
         fullWidth
         multiline
         required
         rows={4}
       />
-      <Button type="submit" variant="outlined" onClick={handleSubmit}>
+      <Button type="submit" variant="outlined">
         Submit
       </Button>
     </form>
