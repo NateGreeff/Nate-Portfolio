a0a1fe164b96cb01334b38bbcd909da13e3ea3fb
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-15 18:05:52 -0700

    Portfolio section work

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,5 +1,24 @@
+import Project from "../../components/Project/Project";
+import projectData from "../../assets/data/myProjects.js";
+
+import "./Portfolio.css";
+
 export default function Portfolio() {
-    return (
-        <p>Portfolio</p>
-    )
+  return (
+    <>
+      <section className="projectContainer">
+        {projectData.map((project, idx) => {
+          return (
+            <Project
+              key={idx}
+              name={project.name}
+              image={project.image}
+              github={project.github}
+              live={project.live}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
