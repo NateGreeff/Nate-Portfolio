a5a4e21f364c603513f60ed9bc6efc8ac38ec1f2
Nathan Greeff <nathangreeff@hotmail.com>
2023-11-17 22:37:53 -0700

    starting resume section

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,5 +1,32 @@
+import './Resume.css';
+
+import './Resume.css';
+
 export default function Resume() {
+    const skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Node.js",
+        "Express",
+        "SQL",
+        "MongoDB",
+        "Python",
+        "Git",
+        "GitHub"
+    ];
+
     return (
-        <p></p>
-    )
+        <div className="resume">
+            <div className="skills background">
+                <h2>Skills</h2>
+                <ul>
+                    {skills.map((skill, index) => (
+                        <li key={index}>{skill}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
