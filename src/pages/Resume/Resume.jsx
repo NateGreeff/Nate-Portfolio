2856e27f66c8093193bd9d9a2db57dcf4333bbdf
Nathan Greeff <nathangreeff@hotmail.com>
2023-12-26 15:41:35 -0700

    Updated technologies

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,33 +1,56 @@
-import './Resume.css';
-import { IoLogoHtml5 , IoLogoCss3 , IoLogoJavascript , IoLogoReact , IoLogoNodejs , IoLogoPython , IoLogoGithub ,} from "react-icons/io5";
+import "./Resume.css";
+import {
+  IoLogoHtml5,
+  IoLogoCss3,
+  IoLogoJavascript,
+  IoLogoReact,
+  IoLogoNodejs,
+  IoLogoPython,
+  IoLogoGithub,
+  IoLogoSass,
+} from "react-icons/io5";
 import { DiMysql } from "react-icons/di";
-import { SiMongodb , SiGit } from "react-icons/si";
-
+import {
+  SiMongodb,
+  SiGit,
+  SiPostgresql,
+  SiJquery,
+  SiBootstrap,
+  SiApollographql,
+} from "react-icons/si";
 
 export default function Resume() {
-    const skills = [
-        { technology: "HTML" , icon: <IoLogoHtml5 /> },
-        { technology: "CSS" , icon: <IoLogoCss3 /> },
-        { technology: "JavaScript" , icon: <IoLogoJavascript /> },
-        { technology: "React" , icon: <IoLogoReact /> },
-        { technology: "Node.js" , icon: <IoLogoNodejs /> },
-        { technology: "SQL" , icon: <DiMysql /> },
-        { technology: "MongoDB" , icon: <SiMongodb /> },
-        { technology: "Python" , icon: <IoLogoPython /> },
-        { technology: "Git" , icon:<SiGit /> },
-        { technology: "GitHub" , icon: <IoLogoGithub /> }
-    ];
-    return (
-        <div className="resume">
-            {/* <a href="" download><button>Download Resume</button></a> */}
-            <div className="skills background">
-                <h2>tech skills</h2>
-                <div className='listedSkills'>
-                    {skills.map((skill, index) => (
-                        <div className='skill' key={index}>{skill.icon}{skill.technology}</div>
-                    ))}
-                </div>
+  const skills = [
+    { technology: "HTML", icon: <IoLogoHtml5 /> },
+    { technology: "CSS", icon: <IoLogoCss3 /> },
+    { technology: "Sass", icon: <IoLogoSass /> },
+    { technology: "Bootstrap", icon: <SiBootstrap /> },
+    { technology: "JavaScript", icon: <IoLogoJavascript /> },
+    { technology: "jQuery", icon: <SiJquery /> },
+    { technology: "Python", icon: <IoLogoPython /> },
+    { technology: "React", icon: <IoLogoReact /> },
+    { technology: "Node.js", icon: <IoLogoNodejs /> },
+    { technology: "MySQL", icon: <DiMysql /> },
+    { technology: "PostgreSQL", icon: <SiPostgresql /> },
+    { technology: "MongoDB", icon: <SiMongodb /> },
+    { technology: "Apollo", icon: <SiApollographql /> },
+    { technology: "Git", icon: <SiGit /> },
+    { technology: "GitHub", icon: <IoLogoGithub /> },
+  ];
+  return (
+    <div className="resume">
+      {/* <a href="" download><button>Download Resume</button></a> */}
+      <div className="skills background">
+        <h2>tech skills</h2>
+        <div className="listedSkills">
+          {skills.map((skill, index) => (
+            <div className="skill" key={index}>
+              {skill.icon}
+              {skill.technology}
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
