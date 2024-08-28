76bed328afff08244770de5b3a43331655437664
Nathan Greeff <nathangreeff@hotmail.com>
2024-08-28 13:44:27 -0600

    Removed non-hosted projects

--- src/assets/data/myProjects.js
+++ src/assets/data/myProjects.js
@@ -1,21 +1,9 @@
 const projectData = [
     {
-        name: "Caption This",
-        live: "https://caption-this-production.up.railway.app/",
-        github: "https://github.com/Greeny467/caption-this",
-        image: "https://i.ibb.co/fxwcrf6/Screenshot-2023-12-26-151813.png"
-    },
-    {
         name: "Utah Real Estate Admin Rules",
         live: "https://re-law-lookup-production-6926.up.railway.app/",
         github: "https://github.com/Jelu113/RE-law-lookup",
         image: "https://raw.githubusercontent.com/Jelu113/RE-law-lookup/main/public/img/HomePage.jpg"
-    },
-    {
-        name: "The Date Planner",
-        live: "https://greeny467.github.io/date-planning-project-website/",
-        github: "https://github.com/Greeny467/date-planning-project-website",
-        image: "https://user-images.githubusercontent.com/21099065/258675578-27000e63-5d47-4c2e-b32a-2a2afaa1b49b.png"
     },
     {
         name: "The Coding Quiz",
